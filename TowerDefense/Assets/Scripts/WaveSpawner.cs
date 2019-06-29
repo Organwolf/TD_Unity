a90e197bb0e49654ea5ee9dec860c42f9c38d0e0
Aron <aron.polner@gmail.com>
2019-06-30 00:44:36 +0200

    End of E03 - Wave Spawner

---
+++ TowerDefense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public float timeBetweenWaves = 5f;
+    public Transform spawnPoint;
+    public Text waveCountdownTex;
+
+    private float countdown = 2f;
+    private int waveIndex = 1;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        waveCountdownTex.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave ()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}

fa0a852c179a318da6e01c7db7de743cd98d48c2
Aron Polner <aron.polner@gmail.com>
2019-07-03 11:08:24 +0200

    Hover color added to Node prefab

---
+++ TowerDefense/Assets/Node.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+// Keeps track of if something is on the node
+// user input, and building something on top of it.
+public class Node : MonoBehaviour
+{
+
+    [SerializeField] Color hoverColor;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    // Unity callbacks
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}

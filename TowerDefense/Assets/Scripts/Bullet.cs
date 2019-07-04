9e54500837786259eed5878a8d4a5d368ef30d31
Aron <aron.polner@gmail.com>
2019-07-04 10:29:01 +0200

    End of E10 - Missiles

--- TowerDefense/Assets/Scripts/Bullet.cs
+++ TowerDefense/Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
 
     [SerializeField] GameObject impactEffect;
     [SerializeField] float speed = 70f;
+    public float explosionRaius = 0f;
 
     public void Seek(Transform _target)
     {
@@ -31,6 +32,7 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 
     }
 
@@ -39,7 +41,39 @@ public class Bullet : MonoBehaviour
         GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effectIns, 1f);
 
+        if(explosionRaius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(target.gameObject);
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRaius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage (Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRaius);
     }
 }
